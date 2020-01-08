7e5fab7a0ded320e9771803775807eab7efa139e
vladrevers <vlad.k.o.vl66@gmail.com>
2020-01-08 16:25:23 +0200

    Add provider: MET Norway
    Change-Id: I3b5eda032ca4f58ead861808e801ed69322c1ba3

--- src/org/omnirom/omnijaws/Config.java
+++ src/org/omnirom/omnijaws/Config.java
@@ -38,11 +38,34 @@ public class Config {
     public static final String PREF_KEY_UPDATE_ERROR = "update_error";
 
     public static AbstractWeatherProvider getProvider(Context context) {
-        return new OpenWeatherMapProvider(context);
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(context);
+
+        switch (prefs.getString(PREF_KEY_PROVIDER, "0"))
+        {
+            case "0":
+                return new OpenWeatherMapProvider(context);
+            case "1":
+                return new METNorwayProvider(context);
+            default:
+                return new OpenWeatherMapProvider(context);
+        }
     }
 
     public static String getProviderId(Context context) {
-        return "OpenWeatherMap";
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(context);
+
+        String provider = prefs.getString(PREF_KEY_PROVIDER, "0");
+        switch (provider)
+        {
+            case "0":
+                return "OpenWeatherMap";
+            case "1":
+                return "MET Norway";
+            default:
+                return "OpenWeatherMap";
+        }
     }
 
     public static boolean isMetric(Context context) {
